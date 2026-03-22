608749ebd0f47c5e407bd048c229ed1609019515
KarkRio <termuxkark@gmail.com>
2026-03-22 06:21:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,14 @@
-const CACHE = "cerebro-v1";
-const ASSETS = ["./index.html", "./manifest.json"];
+const CACHE = 'cerebro-v7';
+const ASSETS = ['./index.html', './manifest.json'];
 
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html'))));
 });
